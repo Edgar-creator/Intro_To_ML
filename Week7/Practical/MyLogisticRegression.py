f3b61aa011f0cf23e9da7b728bdbed5b33b247e9
Edgar-creator <edgarbabayan374@gmail.com>
2020-08-15 11:30:37 +0400

    Pr

--- Week7/Practical/MyLogisticRegression.py
+++ Week7/Practical/MyLogisticRegression.py
@@ -4,7 +4,7 @@ import numpy as np
 
 
 class MyLogisticRegression():
-    def __init__(self,epsilon=1e-6,step_size=1e-4,max_steps=1000,lambd=0,fit_intercept=False):
+    def __init__(self,epsilon=1e-6,step_size=1e-4,max_steps=1000,lambd=0,fit_intercept=True):
         self.epsilon = epsilon
         self.step_size = step_size
         self.max_steps = max_steps
@@ -17,29 +17,22 @@ class MyLogisticRegression():
         return 1/(1+np.exp(-X))
     
     def new(self,X):
-        if self.fit_intercept is True and "for_intercept" not in X.columns:
-            x=X.copy()
-            x["for_intercept"]=1
-            x = x[[x.columns[-1]] + list(x.columns[:-1])]
-            return x
+        X = np.array(X)
+        if self.fit_intercept is True:
+            X1 = np.insert(X, 0, 1, axis=1)
+            return X1
         return X
     
     def logistic_func(self,X,beta):
-        X = self.new(X)
         return 1/(1+np.exp(np.dot(np.array(-beta),X.T)))
  
     
     def cost_func(self,X, Y,beta):
         cost=0
         regular=0
-        X1 = self.new(X)
-        if  X1.shape[1]==X.shape[1]:
-            cost=np.dot(-Y,np.log(self.logistic_func(X,beta)))-np.dot((1-Y),np.log(1-self.logistic_func(X,beta)))+ ((self.lambd/(2*X.shape[1])))*(beta**2).sum()
-            return cost/X.shape[0]
-        else:
-            cost=np.dot(-Y,np.log(self.logistic_func(X1,beta)))-np.dot((1-Y),np.log(1-self.logistic_func(X1,beta)))
-            regular = ((self.lambd/(2*X.shape[1])))*(beta[1:]**2).sum()
-            return cost/X1.shape[0] + regular/(X1.shape[0]-1)
+        cost=np.dot(-Y,np.log(self.logistic_func(X,beta)))-np.dot((1-Y),np.log(1-self.logistic_func(X,beta)))+ ((self.lambd/(2*X.shape[1])))*(beta**2).sum()
+        return cost/X.shape[0]
+     
     
     def gradient(self,X, Y,beta):
 
@@ -51,31 +44,29 @@ class MyLogisticRegression():
         beta= np.zeros(X1.shape[1])
 
         for i in range(self.max_steps):
-            if X1.shape[1]==X.shape[1]:
-                old_cost = self.cost_func(X, Y,beta)
-            
-                beta -= self.step_size*self.gradient(X, Y,beta)
-                if abs(self.cost_func(X,Y,beta) - old_cost) <= self.epsilon:
-                    print("Gradient Descent converged at %s step" % i)
-                    break
-            else:
-                old_cost = self.cost_func(X, Y,beta)
-                beta -= self.step_size*self.gradient(X1, Y, beta)
-                if abs(self.cost_func(X1,Y,beta) - old_cost) <= self.epsilon/3:
-                    print("Gradient Descent converged at %s step" % i)
-                    break
+            old_cost = self.cost_func(X1, Y,beta)
+            beta -= self.step_size*self.gradient(X1, Y, beta)
+            if abs(self.cost_func(X1,Y,beta) - old_cost) <= self.epsilon/5:
+                print("Gradient Descent converged at %s step" % i)
+                break
+        self.beta = beta
+                 
         if self.fit_intercept is True:
-            self.intercept_=beta[0]
-            self.coef_ = beta[1:]
+            self.intercept_=self.beta[0]
+            self.coef_ = self.beta[1:]
         else:
             self.intercept_ = []
-            self.coef_ = beta
+            self.coef_ = self.beta
+        self.by_importance_coef_ = sorted(abs(self.coef_))
     
     def fit(self,X,Y):
         self.gradient_descent(X, Y)
         
+        
     def predict(self,X):
-        return np.where(V.logistic_func(xtest,beta)>0.5,1,0)
+        X = self.new(X)
+        return np.where(self.logistic_func(X,self.beta)>0.5,1,0)
+   
     
     
     
